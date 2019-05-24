68f513bbb55e87eb6966052e811acab1d63971af
Brooks Patton <brooks@brookzerker.com>
2019-05-24 14:47:13 -0600

    added link to twitch page and resume

--- src/top-nav/top-nav.js
+++ src/top-nav/top-nav.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import brooksImage from "./brooks-01.svg";
+import brooksResume from "./brookspatton_resume_may_2019.pdf";
+import externalIcon from "./External.svg";
 
 import "./top-nav.css";
 
@@ -29,6 +31,14 @@ function TopNav({ location }) {
         >
           Work Experience
         </Link>
+        <a href="https://www.twitch.tv/brookzerker" target="window">
+          Twitch.tv Channel{" "}
+          <img src={externalIcon} alt="this link leads off the page" />
+        </a>
+        <a href={brooksResume} target="window">
+          Download Resume{" "}
+          <img src={externalIcon} alt="this link leads to off the page" />
+        </a>
       </nav>
     </section>
   );
